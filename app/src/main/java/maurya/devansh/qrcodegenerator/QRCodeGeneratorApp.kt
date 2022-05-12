6ffdbe8df604df80e7349901f2e85ed7c29cd89c
Devansh-Maurya <devansh233@gmail.com>
2022-05-12 14:04:53 +0530

    Add QRCodeGeneratorApp application class

---
+++ app/src/main/java/maurya/devansh/qrcodegenerator/QRCodeGeneratorApp.kt
@@ -0,0 +1,18 @@
+package maurya.devansh.qrcodegenerator
+
+import android.app.Application
+import io.appwrite.Client
+
+/**
+ * Created by devansh on 12/05/22.
+ */
+
+class QRCodeGeneratorApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        val client = Client(this)
+            .setEndpoint("${BuildConfig.APPWRITE_BASE_URL}/v1")
+            .setProject(BuildConfig.APPWRITE_PROJECT_ID)
+            .setSelfSigned(true)
+    }
+}
