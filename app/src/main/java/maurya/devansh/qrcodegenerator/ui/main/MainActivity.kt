115ac3dc28a21b4659c5162bfd0d1d15329241c9
Devansh-Maurya <devansh233@gmail.com>
2022-05-12 23:38:29 +0530

    Add null and empty check before QR generation

--- app/src/main/java/maurya/devansh/qrcodegenerator/ui/main/MainActivity.kt
+++ app/src/main/java/maurya/devansh/qrcodegenerator/ui/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package maurya.devansh.qrcodegenerator.ui.main
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import maurya.devansh.qrcodegenerator.databinding.ActivityMainBinding
 import maurya.devansh.qrcodegenerator.ui.qrcode.QrCodeActivity
@@ -14,12 +15,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btGenerateQr.setOnClickListener {
-            startActivity(
-                QrCodeActivity.getStartIntent(
-                    this,
-                    binding.editText.text?.toString().orEmpty()
-                )
-            )
+            val qrText = binding.editText.text?.toString()
+
+            if (!qrText.isNullOrEmpty()) {
+                startActivity(QrCodeActivity.getStartIntent(this, qrText))
+            } else {
+                Toast.makeText(this, "Enter some text", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
