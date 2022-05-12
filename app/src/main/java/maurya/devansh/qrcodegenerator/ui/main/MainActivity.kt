84e7bdd3ace3756d4fd83d9175390ad44e6ec41e
Devansh-Maurya <devansh233@gmail.com>
2022-05-12 21:34:49 +0530

    Load and display generated qr code using Glide

--- app/src/main/java/maurya/devansh/qrcodegenerator/ui/main/MainActivity.kt
+++ app/src/main/java/maurya/devansh/qrcodegenerator/ui/main/MainActivity.kt
@@ -3,6 +3,7 @@ package maurya.devansh.qrcodegenerator.ui.main
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import maurya.devansh.qrcodegenerator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +14,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        viewModel.qrCodeLiveData.observe(this) {
+            Glide.with(this)
+                .load(it)
+                .into(binding.ivQr)
+        }
+
+        binding.btGenerateQr.setOnClickListener {
+            viewModel.getQrCodeUrl(binding.editText.text?.toString().orEmpty())
+        }
     }
 }
--- app/src/main/java/maurya/devansh/qrcodegenerator/ui/main/MainViewModel.kt
+++ app/src/main/java/maurya/devansh/qrcodegenerator/ui/main/MainViewModel.kt
@@ -1,6 +1,10 @@
 package maurya.devansh.qrcodegenerator.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import maurya.devansh.qrcodegenerator.BuildConfig
+import maurya.devansh.qrcodegenerator.data.model.QrCodeImageUrl
 import maurya.devansh.qrcodegenerator.data.repository.QrCodeRepository
 
 /**
@@ -10,4 +14,24 @@ import maurya.devansh.qrcodegenerator.data.repository.QrCodeRepository
 class MainViewModel(
     private val qrCodeRepository: QrCodeRepository
 ) : ViewModel() {
+
+    private val _qrCodeLiveData = MutableLiveData<QrCodeImageUrl>()
+    val qrCodeLiveData: LiveData<QrCodeImageUrl>
+        get() = _qrCodeLiveData
+
+//    fun generateQrCode(text: String) {
+//        viewModelScope.launch {
+//            qrCodeRepository.getQrCode(text)
+//                .catch { e ->
+//                    Log.e("ApiError", e.localizedMessage.orEmpty())
+//                }
+//                .collect {
+//                    _qrCodeLiveData.value = it.bytes()
+//                }
+//        }
+//    }
+
+    fun getQrCodeUrl(text: String) {
+        _qrCodeLiveData.value = "${BuildConfig.APPWRITE_BASE_URL}v1/avatars/qr?text=${text}"
+    }
 }
