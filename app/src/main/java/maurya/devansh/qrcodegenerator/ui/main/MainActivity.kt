202fd5c47d1e087efe06e1a5fa0e6707d60847f8
Devansh Maurya <Devansh233@gmail.com>
2022-05-12 21:45:21 +0530

    Feat/qr code generation (#2)
    * Add UI to enter text for QR
    
    * Add viewbinding
    
    * Add HTTP logging interceptor
    
    * Add Glide dependency
    
    * Load and display generated qr code using Glide
    
    * Delete non-required network call related classes

--- app/src/main/java/maurya/devansh/qrcodegenerator/ui/main/MainActivity.kt
+++ app/src/main/java/maurya/devansh/qrcodegenerator/ui/main/MainActivity.kt
@@ -3,14 +3,26 @@ package maurya.devansh.qrcodegenerator.ui.main
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import maurya.devansh.qrcodegenerator.R
+import com.bumptech.glide.Glide
+import maurya.devansh.qrcodegenerator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val viewModel by viewModels<MainViewModel> { MainViewModelFactory() }
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        viewModel.qrCodeLiveData.observe(this) {
+            Glide.with(this)
+                .load(it)
+                .into(binding.ivQr)
+        }
+
+        binding.btGenerateQr.setOnClickListener {
+            viewModel.getQrCodeUrl(binding.editText.text?.toString().orEmpty())
+        }
     }
 }
--- app/src/main/java/maurya/devansh/qrcodegenerator/ui/main/MainViewModel.kt
+++ app/src/main/java/maurya/devansh/qrcodegenerator/ui/main/MainViewModel.kt
@@ -1,13 +1,22 @@
 package maurya.devansh.qrcodegenerator.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import maurya.devansh.qrcodegenerator.data.repository.QrCodeRepository
+import maurya.devansh.qrcodegenerator.BuildConfig
+import maurya.devansh.qrcodegenerator.data.model.QrCodeImageUrl
 
 /**
  * Created by devansh on 12/05/22.
  */
 
-class MainViewModel(
-    private val qrCodeRepository: QrCodeRepository
-) : ViewModel() {
+class MainViewModel : ViewModel() {
+
+    private val _qrCodeLiveData = MutableLiveData<QrCodeImageUrl>()
+    val qrCodeLiveData: LiveData<QrCodeImageUrl>
+        get() = _qrCodeLiveData
+
+    fun getQrCodeUrl(text: String) {
+        _qrCodeLiveData.value = "${BuildConfig.APPWRITE_BASE_URL}v1/avatars/qr?text=${text}"
+    }
 }
