235e7d29b1d3dd61e30b399775d6f1a38facde44
Devansh-Maurya <devansh233@gmail.com>
2022-05-12 21:58:55 +0530

    Add QrCodeActivity to display QR code

---
+++ app/src/main/java/maurya/devansh/qrcodegenerator/ui/qrcode/QrCodeActivity.kt
@@ -0,0 +1,36 @@
+package maurya.devansh.qrcodegenerator.ui.qrcode
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import maurya.devansh.qrcodegenerator.R
+import maurya.devansh.qrcodegenerator.data.model.QrCodeImageUrl
+import maurya.devansh.qrcodegenerator.databinding.ActivityQrCodeBinding
+
+
+class QrCodeActivity : AppCompatActivity() {
+
+    companion object {
+        private const val INTENT_EXTRA_QR_CODE_URL = "qr_code_url"
+
+        fun getStartIntent(context: Context, qrCodeUrl: QrCodeImageUrl): Intent =
+            Intent(context, QrCodeActivity::class.java).apply {
+                putExtra(INTENT_EXTRA_QR_CODE_URL, qrCodeUrl)
+            }
+    }
+
+    private val binding by lazy { ActivityQrCodeBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val qrCodeUrl = intent?.getStringExtra(INTENT_EXTRA_QR_CODE_URL)
+        Glide.with(this)
+            .load(qrCodeUrl)
+            .placeholder(R.drawable.placeholder)
+            .into(binding.ivQrCode)
+    }
+}
