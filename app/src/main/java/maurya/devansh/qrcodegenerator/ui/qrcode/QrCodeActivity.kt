b21914b94b6f3fdb7c8d3280193e478bbca34881
Devansh-Maurya <devansh233@gmail.com>
2022-05-12 22:39:53 +0530

    Add up button

--- app/src/main/java/maurya/devansh/qrcodegenerator/ui/qrcode/QrCodeActivity.kt
+++ app/src/main/java/maurya/devansh/qrcodegenerator/ui/qrcode/QrCodeActivity.kt
@@ -3,6 +3,7 @@ package maurya.devansh.qrcodegenerator.ui.qrcode
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import maurya.devansh.qrcodegenerator.R
@@ -27,11 +28,22 @@ class QrCodeActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         title = "Your QR Code"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val qrCodeUrl = intent?.getStringExtra(INTENT_EXTRA_QR_CODE_URL)
         Glide.with(this)
             .load(qrCodeUrl)
             .thumbnail(Glide.with(this).load(R.drawable.placeholder))
             .into(binding.ivQrCode)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
