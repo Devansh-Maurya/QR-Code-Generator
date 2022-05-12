efaabae7835aebc59da6827c81c9ea85bb60772d
Devansh-Maurya <devansh233@gmail.com>
2022-05-12 21:39:55 +0530

    Delete non-required network call related classes

--- app/src/main/java/maurya/devansh/qrcodegenerator/ui/main/MainViewModel.kt
+++ app/src/main/java/maurya/devansh/qrcodegenerator/ui/main/MainViewModel.kt
@@ -5,31 +5,16 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import maurya.devansh.qrcodegenerator.BuildConfig
 import maurya.devansh.qrcodegenerator.data.model.QrCodeImageUrl
-import maurya.devansh.qrcodegenerator.data.repository.QrCodeRepository
 
 /**
  * Created by devansh on 12/05/22.
  */
 
-class MainViewModel(
-    private val qrCodeRepository: QrCodeRepository
-) : ViewModel() {
+class MainViewModel : ViewModel() {
 
     private val _qrCodeLiveData = MutableLiveData<QrCodeImageUrl>()
     val qrCodeLiveData: LiveData<QrCodeImageUrl>
         get() = _qrCodeLiveData
-
-//    fun generateQrCode(text: String) {
-//        viewModelScope.launch {
-//            qrCodeRepository.getQrCode(text)
-//                .catch { e ->
-//                    Log.e("ApiError", e.localizedMessage.orEmpty())
-//                }
-//                .collect {
-//                    _qrCodeLiveData.value = it.bytes()
-//                }
-//        }
-//    }
 
     fun getQrCodeUrl(text: String) {
         _qrCodeLiveData.value = "${BuildConfig.APPWRITE_BASE_URL}v1/avatars/qr?text=${text}"
